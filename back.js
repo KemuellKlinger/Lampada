c191608ef0bac5a1599bcb682dd280d1f029f5f7
Kemuell Klinger <99361735+kemuell411@users.noreply.github.com>
2023-10-19 20:12:37 -0300

    Update back.js

--- back.js
+++ back.js
@@ -1,10 +1,3 @@
-function atraso (callback){
-    setTimeout(callback, 1000);
-}
-function print(){
-    console.log("Deu certo!")
-}
-
 function interruptor(){
     var elemento = document.getElementById("lampada");
     var corpo = document.getElementById("corpo");
@@ -20,16 +13,4 @@ function interruptor(){
         corpo.style.backgroundColor = "black";
         paragrafo.style.color = "white"
 }
-console.log(corpo)
 }
-
-
-// function mudar(){
-//     var corpo = document.getElementById("corpo").value;
-//     var verde = "linear-gradient(to right, #30AFDB, #30DBD1, #30AFDB )";
-//     if ((){
-//         corpo.style.backgroundImage = "linear-gradient(to right, #30AFDB, #30DBD1, #32CD32);";
-//     }else{
-//         corpo.style.backgroundImage = "linear-gradient(to right, #99DBC1, #30DBD1, #30AFDB);"
-//     }
-// 
